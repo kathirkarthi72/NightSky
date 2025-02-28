d0bfc09da6377b8271091d802a10ad151153bdfd
Kathiresan Murugan <kathirkarthi72@gmail.com>
2025-02-28 16:59:44 +0530

    public access for init viewmodel

--- Sources/NightSky/SkyView.swift
+++ Sources/NightSky/SkyView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct SkyView: View {
-   private var mode: ColorScheme
+    private var mode: ColorScheme
     
     public init(mode: ColorScheme) {
         self.mode = mode
     }
-        
+    
     public var body: some View {
         if mode == .dark {
             NightSkyBaseView(vm: .init())
@@ -28,14 +28,16 @@ public struct SkyView: View {
 @available(iOS 13.0, *)
 final public class NightSkyBaseViewModel: ObservableObject {
     
+    public init() {}
+    
     private let defaultSlide: String = "SLIDE-0"
     @Published var currentSlide: String = "SLIDE-0"
-//    @Published var angle: Angle = .zero
+    //    @Published var angle: Angle = .zero
     
-//    @Published var rotateTimer = Timer.publish(every: 0.2, on: .main, in: RunLoop.Mode.common).autoconnect()
+    //    @Published var rotateTimer = Timer.publish(every: 0.2, on: .main, in: RunLoop.Mode.common).autoconnect()
     
     @Published var starTimer = Timer.publish(every: 1, on: .main, in: RunLoop.Mode.common).autoconnect()
-
+    
     private var starNames: [String] = ["SLIDE-0",
                                        "SLIDE-1",
                                        "SLIDE-2",
@@ -59,13 +61,13 @@ final public class NightSkyBaseViewModel: ObservableObject {
         } else {
             currentSlide = defaultSlide
         }
-//        print(currentSlide)
+        //        print(currentSlide)
         
-//       let resetStar = DispatchWorkItem(block: {
-//           self.currentSlide = self.defaultSlide
-//        })
-//
-//        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2, execute: resetStar)
+        //       let resetStar = DispatchWorkItem(block: {
+        //           self.currentSlide = self.defaultSlide
+        //        })
+        //
+        //        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2, execute: resetStar)
     }
     
 }
@@ -74,29 +76,33 @@ final public class NightSkyBaseViewModel: ObservableObject {
 public struct NightSkyBaseView: View {
     @ObservedObject var vm: NightSkyBaseViewModel
     
+    public init(vm: NightSkyBaseViewModel) {
+        self.vm = vm
+    }
+    
     public var body: some View {
         GeometryReader { proxy in
             Image(ImageResource(name: vm.currentSlide, bundle: .module))
                 .resizable()
                 .aspectRatio(contentMode: .fit) // Preserve aspect ratio
                 .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
-    //            .rotationEffect(-vm.angle)
-//                .frame(width: 1400, height: 1400) // Set desired frame size
+            //            .rotationEffect(-vm.angle)
+            //                .frame(width: 1400, height: 1400) // Set desired frame size
         }
-   
+        
         //            .background(content: {
         //                Color.black
         //            })
-//            .onReceive(vm.rotateTimer) { refresh in
-//                withAnimation {
-//                    self.vm.angle.degrees += 0.1
-//                }
-//            }
-            .onReceive(vm.starTimer) { refresh in
-                withAnimation(.linear, {
-                    self.vm.changeSlide()
-                })
-            }
+        //            .onReceive(vm.rotateTimer) { refresh in
+        //                withAnimation {
+        //                    self.vm.angle.degrees += 0.1
+        //                }
+        //            }
+        .onReceive(vm.starTimer) { refresh in
+            withAnimation(.linear, {
+                self.vm.changeSlide()
+            })
+        }
         
     }
 }
